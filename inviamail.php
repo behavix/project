58debee34619db688ac1f01ae6214a636b9da853
behavix <144628391+behavix@users.noreply.github.com>
2023-11-13 21:49:08 +0100

    Update inviamail.php request method

--- inviamail.php
+++ inviamail.php
@@ -1,22 +1,23 @@
 <?php
-$to = "massimiliano.carraro@behavix.it";
-$headers = "From: clientehainserito@behavix.it" . "\r\n" .
-"CCn: engioturri@gmail.com";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $nome = $_POST['nome'];
+  $emailcliente = $_POST['mail'];
+  $tel = $_POST['tel'];
+  $richiesta = $_POST['mex'];
 
-$nome = $_POST['nome'];
-$emailcliente = $_POST['mail'];
-$tel = $_POST['tel'];
-$richiesta = $_POST['mex'];
+  $messaggio = $nome . "\r\n" . "\r\n" .  $richiesta . "\r\n" . "\r\n" .  $tel;
 
-$messaggio = $nome . "\r\n" . "\r\n" .  $richiesta . "\r\n" . "\r\n" .  $tel;
+  $to = "massimiliano.carraro@behavix.it";
+  $headers = "From: clientehainserito@behavix.it" . "\r\n" .
+    "Ccn: engioturri@gmail.com";
 
-// Modified line: Using the `mail` function correctly
-$retval = mail($to, $emailcliente, $messaggio, $headers);
+  $retval = mail($to, $emailcliente, $messaggio, $headers);
 
-if( $retval == true ) {
+  if ($retval == true) {
     echo "Message sent successfully...";
-}else {
+  } else {
     echo "Message could not be sent...";
+  }
 }
 
 ?>
