119d0614815a82550cebfa3bbe8765effee917fb
behavix <144628391+behavix@users.noreply.github.com>
2023-11-13 21:03:22 +0100

    Update inviamail.php mail

--- inviamail.php
+++ inviamail.php
@@ -1,7 +1,7 @@
 <?php
 $to = "massimiliano.carraro@behavix.it";
 $headers = "From: clientehainserito@behavix.it" . "\r\n" .
-"CC: engioturri@gmail.com";
+"CCn: engioturri@gmail.com";
 
 $nome = $_POST['nome'];
 $emailcliente = $_POST['mail'];
@@ -10,12 +10,13 @@ $richiesta = $_POST['mex'];
 
 $messaggio = $nome . "\r\n" . "\r\n" .  $richiesta . "\r\n" . "\r\n" .  $tel;
 
-$retval = mail($to,$emailcliente,$messaggio,$headers);
+// Modified line: Using the `mail` function correctly
+$retval = mail($to, $emailcliente, $messaggio, $headers);
 
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
+if( $retval == true ) {
+    echo "Message sent successfully...";
+}else {
+    echo "Message could not be sent...";
+}
 
 ?>
